74f6560d17942afb8b309b4b6bca7d54f727f240
AjayBS <bsajay.22@gmail.com>
2020-01-26 20:55:18 -0600

    Initial commit

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public GameObject parachute;
+    public float drag = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            parachute.gameObject.SetActive(true);
+            this.GetComponent<Rigidbody>().drag = drag;
+        }
+
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Rotate(0, 10, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Rotate(0, -10, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/WindArea.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindArea : MonoBehaviour
+{
+    public float Strength;
+    public Vector3 WindDirection;
+
+    void OnTriggerStay(Collider body)
+    {
+        if(body.gameObject.CompareTag("Player"))
+        {
+            Rigidbody colRigidbody = body.GetComponent<Rigidbody>();
+            if (colRigidbody != null)
+            {
+                colRigidbody.AddForce(WindDirection * Strength);
+            }
+        }      
+    }
+
+    private void OnTriggerExit(Collider body)
+    {
+        if (body.gameObject.CompareTag("Player"))
+        {
+            Rigidbody colRigidbody = body.GetComponent<Rigidbody>();
+            colRigidbody.velocity = Vector3.zero;
+            colRigidbody.angularVelocity = Vector3.zero;
+        }
+        
+    }
+}
